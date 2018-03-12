2a87d08c08979c9ed7cb77b16d60b30af9064765
Matthew Sobieray <matt@hyfyn.com>
2018-03-11 21:29:24 -0600

    removed firebase config

--- src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-export default {
-  apiKey: "AIzaSyAKPznXszuDabDs5RlNLRDBCu2evmY9fDY",
-  authDomain: "carin-3da86.firebaseapp.com",
-  databaseURL: "https://carin-3da86.firebaseio.com",
-  projectId: "carin-3da86",
-  storageBucket: "",
-  messagingSenderId: "332544731833"
-};
