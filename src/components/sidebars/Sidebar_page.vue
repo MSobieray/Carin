64abd823778b14b3608f56778266c51fedcec64a
Matthew Sobieray <matt@hyfyn.com>
2020-03-28 18:13:45 -0600

    closes #17 - add form validation

--- src/components/sidebars/Sidebar_page.vue
+++ src/components/sidebars/Sidebar_page.vue
@@ -13,22 +13,26 @@
       </v-list-item>
     </v-list-item-group>
     <v-list-item-group v-else>
-      <v-list-item>
-        <v-list-item-content>
-          <v-text-field
-            v-if="editable"
-            v-model="pageName"
-            :placeholder="page.name"
-          >
-          </v-text-field>
-        </v-list-item-content>
-        <v-list-item-icon>
-          <v-icon @click="save">save</v-icon>
-        </v-list-item-icon>
-        <v-list-item-icon>
-          <v-icon @click="edit">cancel</v-icon>
-        </v-list-item-icon>
-      </v-list-item>
+      <v-form v-model="valid">
+        <v-list-item>
+          <v-list-item-content>
+            <v-text-field
+              v-if="editable"
+              v-model="pageName"
+              :placeholder="page.name"
+              :rules="nameRules"
+              required
+            >
+            </v-text-field>
+          </v-list-item-content>
+          <v-list-item-icon>
+            <v-icon :disabled="!valid" @click="save">save</v-icon>
+          </v-list-item-icon>
+          <v-list-item-icon>
+            <v-icon @click="edit">cancel</v-icon>
+          </v-list-item-icon>
+        </v-list-item>
+      </v-form>
     </v-list-item-group>
   </v-list>
 </template>
@@ -40,7 +44,12 @@ export default {
   data() {
     return {
       pageName: "",
-      editable: false
+      editable: false,
+      valid: false,
+      nameRules: [
+        v => !!v || "A page name is required"
+        // v => v.length > 0 || "Name must be less than 10 characters"
+      ]
     };
   },
   computed: {
